324a23a06927e1e0c14d502fe4453c6f333a78e3
Tomasz Paszkowski <tpaszkowski@g24.suse.de>
2013-06-14 12:49:45 +0200

    Remove notification of openvswitch-switch on /etc/quantum/plugins/openvswitch/ovs_quantum_plugin.ini link.
    This is quantum configuration file and there's no reason to restart
    openvswitch-switch.

--- chef/cookbooks/quantum/recipes/common_install.rb
+++ chef/cookbooks/quantum/recipes/common_install.rb
@@ -220,7 +220,6 @@ vlan_end = vlan_start + 2000
 link "/etc/quantum/plugins/openvswitch/ovs_quantum_plugin.ini" do
   to "/etc/quantum/quantum.conf"
   notifies :restart, resources(:service => quantum_agent), :immediately
-  notifies :restart, resources(:service => "openvswitch-switch"), :immediately
 end
 
 template "/etc/quantum/quantum.conf" do
